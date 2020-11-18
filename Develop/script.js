10fe39021c8fdf06df22046b39c90d098085981d
Zihao <zihaozhang1@gmail.com>
2020-11-18 03:02:56 -0500

    timeblock introduced

--- Develop/script.js
+++ Develop/script.js
@@ -23,6 +23,34 @@ $("#hour17 .description").val(localStorage.getItem("hour17"));
 function clock() {
     var currentDate = moment().format('dddd MMMM Do YYYY h:mm:ss a');
     $('#currentDay').html(currentDate);
+    function hourTracker() {
+        //get current number of hours.
+        var currentTime = moment().hour();
+    
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockTime, currentTime)
+    
+            //Hour blocks if past time
+            if (blockTime < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
 }
 
 setInterval(clock, 1000);
