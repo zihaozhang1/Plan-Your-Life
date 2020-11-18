268886065374f4b63b6f5e5dfca5f3f7dab15f39
Zihao <zihaozhang1@gmail.com>
2020-11-18 00:58:16 -0500

    localstorage data added

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,25 @@
+//LocalStorage Data
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+ //saveBtn assigned
+ $(".saveBtn").on("click", function () {
+    //get nearby values.
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //save items in local storage.
+    localStorage.setItem(time, text);
+})
+
 function clock() {
     var currentDate = moment().format('dddd MMMM Do YYYY h:mm:ss a');
     $('#currentDay').html(currentDate);
